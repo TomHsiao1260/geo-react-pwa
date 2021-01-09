73b74267626588b570ce04ff5f8d44d199f5d1f7
Hsiao Yao <tomtogo1260@gmail.com>
2021-01-09 21:12:13 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component, useRef, useEffect} from 'react';
 import * as THREE from "three";
 import logo from './logo.svg';
 import './App.css';
-import Btn from './button';
+import Btn, {height} from './button';
 
 import Procedural from 'procedural-gl';
 import imgMoon from './img/moon.jpg';
@@ -11,13 +11,35 @@ import imgPeak from './img/peak.jpg';
 import balloon from './textures/balloon.png';
 
 import data from './data.json';
-const arr = [];
-const height = [];
+import { v4 as uuidv4 } from 'uuid';
 
-function App(props){
+const arr = [];
+
+data['trkpt'].forEach( i => {
+  arr.push( [i.lon, i.lat] );
+})
+
+const path = {
+  "geometry": {
+    "type": "LineString",
+    "coordinates": arr
+  },
+  "type": "Feature",
+  "id": 0,
+  "properties": {
+    "color": "#0000ff"
+  }
+}
+
+const overlay = {
+  "type": "FeatureCollection",
+  "features": []
+}
+
+function App(){
   const containerRef = useRef(null);
 
-  useEffect(() => {
+  useEffect(async() => {
 
     Procedural.init( {
         container: containerRef.current,
@@ -37,87 +59,30 @@ function App(props){
     Procedural.setRotationControlVisible( true );
     Procedural.setZoomControlVisible( true );
 
+    var target = { latitude: 24.21090, longitude: 121.37088 };
+    Procedural.displayLocation( target );
+
+    height();
+
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    var target = {
+      latitude: 24.20650, longitude: 121.36888,
+      bearing:280, distance: 1000,
+      animationDuration: 2
+    };
+    Procedural.focusOnLocation ( target );
 
   }, []);
-
-  useEffect(() => {
-
-        var target = { latitude: 24.21090, longitude: 121.37088 };
-        Procedural.displayLocation( target );
-        // var target = {
-        //   latitude: 24.20590, longitude: 121.37088,
-        //   angle: 20, bearing: 0, distance: 1000,
-        //   animationDuration: 0.5
-        // };
-        // Procedural.focusOnLocation( target );
-
-        Procedural.onUserInteraction = function () {
-          console.log(props);
-        }
-
-        Procedural.onLocationFocused = function () {
-            console.log( 'Location focused' );
-        };
-
-        data['trkpt'].forEach( (data, i) => {
-          arr.push( [data.lon, data.lat] );
-
-          if (i%100 == 0){
-            const objHeight = {
-              "geometry": {
-                "type": "Point",
-                "coordinates": [data.lon, data.lat]
-              },
-              "type": "Feature",
-              "id": 1,
-              "properties": {
-                "collapseDistance": 1000,
-                "fadeDistance": 5000,
-                "name": Math.floor(data.elev),
-                "background": "blue",
-            
-                "clipping": "object",
-                "color": "white",
-                "padding": 10,
-                "borderRadius": 10,
-                "borderWidth": 1,
-                "anchor": "left",
-                "icon": "eye"
-              }
-            };
-            overlay.features.push( objHeight );
-          }
-        })
-
-        overlay.features.push( path );
-
-        Procedural.addOverlay( overlay );
-
-  }, [props.card])
 
   return (
     <>
     <div ref={containerRef} style={{position: 'absolute', width: '100%', height: '100%'}} ></div>
+    <Btn />
     </>
   )
 
 }
 
-const path = {
-  "geometry": {
-    "type": "LineString",
-    "coordinates": arr
-  },
-  "type": "Feature",
-  "id": 0,
-  "properties": {
-    "color": "#0000ff"
-  }
-}
-
-const overlay = {
-  "type": "FeatureCollection",
-  "features": []
-}
 
 export default App;
--- src/button.js
+++ src/button.js
@@ -3,159 +3,208 @@ import Procedural from 'procedural-gl';
 import imgMoon from './img/moon.jpg';
 import imgPeak from './img/peak.jpg';
 import data from './data.json';
+import { v4 as uuidv4 } from 'uuid';
 
 const arr = [];
 
-function Btn(){
-  data['trkpt'].forEach( i => {
-    arr.push( [i.lon, i.lat] );
-  })
+data['trkpt'].forEach( i => {
+  arr.push( [i.lon, i.lat] );
+})
+
+const path = {
+  "geometry": {
+    "type": "LineString",
+    "coordinates": arr
+  },
+  "type": "Feature",
+  "id": 0,
+  "properties": {
+    "color": "#0000ff"
+  }
+}
+
+const overlayHeight = {
+  "type": "FeatureCollection",
+  "features": []
+}
+overlayHeight.features.push( path );
+
+const overlayTime = {
+  "type": "FeatureCollection",
+  "features": []
+}
+overlayTime.features.push( path );
+
+const overlayNote = {
+  "type": "FeatureCollection",
+  "features": []
+}
+overlayNote.features.push( path );
+
+data['trkpt'].forEach( (data, i) => {
+  if (i%100 == 0){
+    const id = uuidv4();
+    const obj1 = {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [data.lon, data.lat]
+      },
+      "type": "Feature",
+      id,
+      "properties": {
+        "anchorOffset": {
+          "y": 20,
+          "x": 0
+        },
+        "borderRadius": 32,
+        "color": "white",
+        "anchor": "center",
+        "icon": "bell"
+      }
+    }
+    const obj2 = {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [data.lon, data.lat]
+      },
+      "type": "Feature",
+      id,
+      "properties": {
+        "name": Math.floor(data.elev),
+        "collapseDistance": 1500,
+        "fadeDistance": 1500,
+        "anchorOffset": {
+          "y": 60,
+          "x": 0
+        },
+        "borderRadius": 4,
+        "padding": 8,
+        "color": "lavender",
+        "anchor": "center",
+        "clipping": "object",
+        "fontSize": 15,
+      }
+    }
+    overlayHeight.features.push( obj1 );
+    overlayHeight.features.push( obj2 );
+  }
+})
+
+data['waypt'].forEach( (data, i) => {
+  let id = uuidv4();
+  const obj1 = {
+    "geometry": {
+      "type": "Point",
+      "coordinates": [data.lon, data.lat]
+    },
+    "type": "Feature",
+    id,
+    "properties": {
+      "anchorOffset": {
+        "y": 20,
+        "x": 0
+      },
+      "borderRadius": 32,
+      "color": "white",
+      "anchor": "center",
+      "icon": "star"
+    }
+  }
+  const obj2 = {
+    "geometry": {
+      "type": "Point",
+      "coordinates": [data.lon, data.lat]
+    },
+    "type": "Feature",
+    id,
+    "properties": {
+      "name": data.note,
+      "collapseDistance": 1500,
+      "fadeDistance": 1500,
+      "anchorOffset": {
+        "y": 60,
+        "x": 0
+      },
+      "borderRadius": 4,
+      "padding": 8,
+      "color": "lavender",
+      "anchor": "center",
+      "clipping": "object",
+      "fontSize": 15,
+    }
+  }
+  overlayNote.features.push( obj1 );
+  overlayNote.features.push( obj2 );
+
+  id = uuidv4();
+  const obj11 = {
+    "geometry": {
+      "type": "Point",
+      "coordinates": [data.lon, data.lat]
+    },
+    "type": "Feature",
+    id,
+    "properties": {
+      "anchorOffset": {
+        "y": 20,
+        "x": 0
+      },
+      "borderRadius": 32,
+      "color": "white",
+      "anchor": "center",
+      "icon": "home"
+    }
+  }
+  const obj22 = {
+    "geometry": {
+      "type": "Point",
+      "coordinates": [data.lon, data.lat]
+    },
+    "type": "Feature",
+    id,
+    "properties": {
+      "name": data.time,
+      "collapseDistance": 1500,
+      "fadeDistance": 1500,
+      "anchorOffset": {
+        "y": 60,
+        "x": 0
+      },
+      "borderRadius": 4,
+      "padding": 8,
+      "color": "lavender",
+      "anchor": "center",
+      "clipping": "object",
+      "fontSize": 15,
+    }
+  }
+  overlayTime.features.push( obj11 );
+  overlayTime.features.push( obj22 );
+})
+
+function Btn(props){
   return (
     <>
-      <button className="btn btn-outline-light " type="button" style={{position: 'absolute', margin: '5px'}} onClick={() => Procedural.addOverlay( overlay )}>Mark 1</button>
-      <button className="btn btn btn-outline-light" type="button" style={{position: 'absolute', margin: '5px', left: '80px'}} onClick={() => Procedural.addOverlay( overlay_ )}>Mark 2</button>
+      <button className="btn btn-outline-dark " type="button" style={{position: 'absolute', margin: '5px'}} onClick={height}>Height</button>
+      <button className="btn btn btn-outline-dark" type="button" style={{position: 'absolute', margin: '5px', left: '80px'}} onClick={time}>Time</button>
+      <button className="btn btn btn-outline-dark" type="button" style={{position: 'absolute', margin: '5px', left: '150px'}} onClick={note}>Note</button>
     </>
   );
 }
 
-const overlay = {
-  "type": "FeatureCollection",
-  "features": [ {
-    "geometry": {
-      "type": "LineString",
-      "coordinates": arr
-    },
-    "type": "Feature",
-    "id": 0,
-    "properties": {
-      "color": "#0000ff"
-    }
-  }
-]
+export function height(){
+  console.log("HEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHHEIGHT")
+  Procedural.addOverlay( overlayHeight );
 }
 
-const overlay_ = {
-  "type": "FeatureCollection",
-  "features": [ 
-    {
-      "geometry": {
-        "type": "LineString",
-        "coordinates": [
-          [ 121.56, 25.19 ],
-          [ 121.55, 25.20 ]
-        ]
-      },
-      "type": "Feature",
-      "id": 0,
-      "properties": {
-        "color": "#0000ff"
-      }
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 121.56, 25.19 ]
-      },
-      "type": "Feature",
-      "id": 0,
-      "properties": {
-        "anchorOffset": {
-          "y": 70,
-          "x": 0
-        },
-        "borderRadius": 32,
-        "image": imgMoon,
-        "height": 60,
-        "width": 60,
-        "borderWidth": 2,
-        "background": "#ccc"
-      }
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 121.56, 25.19 ]
-      },
-      "type": "Feature",
-      "id": 0,
-      "properties": {
-        "fontSize": 20,
-        "anchorOffset": {
-          "y": 45,
-          "x": 0
-        },
-        "icon": "caret-down"
-      }
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 121.56, 25.19 ]
-      },
-      "type": "Feature",
-      "id": 0,
-      "properties": {
-        "name": "3",
-        "borderRadius": 23,
-        "padding": 5,
-        "fontSize": 13,
-        "background": "#44b6f7",
-        "anchor": "bottom"
-      }
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 121.55, 25.20 ]
-      },
-      "type": "Feature",
-      "id": 1,
-      "properties": {
-        "anchorOffset": {
-          "y": 70,
-          "x": 0
-        },
-        "borderRadius": 32,
-        "image": imgPeak,
-        "height": 60,
-        "width": 60,
-        "borderWidth": 2,
-        "background": "#ccc"
-      }
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 121.55, 25.20 ]
-      },
-      "type": "Feature",
-      "id": 1,
-      "properties": {
-        "fontSize": 20,
-        "anchorOffset": {
-          "y": 45,
-          "x": 0
-        },
-        "icon": "caret-down"
-      }
-    },
-    {
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 121.55, 25.20 ]
-      },
-      "type": "Feature",
-      "id": 1,
-      "properties": {
-        "name": "5",
-        "borderRadius": 23,
-        "padding": 5,
-        "fontSize": 13,
-        "background": "#44b6f7",
-        "anchor": "bottom"
-      }
-    }
-  ]
+function time(){
+  console.log("TIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIME")
+  Procedural.addOverlay( overlayTime );
+}
+
+function note(){
+  console.log("TIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIMETIME")
+  Procedural.addOverlay( overlayNote );
 }
 
 export default Btn;
